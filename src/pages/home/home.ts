49f851ae8473a2839c491093bc310d08f2fafd98
Pierre Thierry <pierre@nothos.net>
2018-02-16 07:04:11 +0100

    Fix type

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  resultat: integer;
+  resultat: number;
 
   constructor(public navCtrl: NavController) {
 
