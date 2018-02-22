7d0921eb5249ab89728545ca3655a38928c1065d
Sheldon <sheldon.warden@gmail.com>
2018-02-22 21:49:37 +0100

    Create dice class

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,12 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   resultat: number;
+  dice: Dice;
 
   constructor(public navCtrl: NavController) {
-
+    this.dice = new Dice(6);
   }
 
   lancer() {
-	this.resultat = Math.floor(Math.random() * 6) + 1;
+	this.resultat = this.dice.roll();
+  }
+}
+
+class Dice {
+  constructor(private faces: number) {}
+  roll() {
+    return  Math.floor(Math.random() * this.faces) + 1;
   }
 }
