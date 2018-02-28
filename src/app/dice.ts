173da209df00cee2f224980baa757a2e9ac52e2c
Pierre Thierry <pierre@nothos.net>
2018-02-28 19:21:48 +0100

    Add explosive dice

--- src/app/dice.ts
+++ src/app/dice.ts
@@ -9,6 +9,24 @@ export class SimpleDice implements Dice {
   }
 }
 
+export class ExplosiveDice implements Dice {
+  private dice: Dice;
+
+  constructor(private faces: numbre) {
+    this.dice = new SimpleDice(faces);
+  }
+
+  roll() {
+    let result = 0;
+    let last_roll = faces;
+
+    while(last_roll == faces) {
+      last_roll = this.dice.roll();
+      result += last_roll;
+    }
+  }
+}
+
 export class Sequence implements Dice {
   constructor(private elements: Dice[]) {}
 
