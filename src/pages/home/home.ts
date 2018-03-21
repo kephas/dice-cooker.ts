137c3c727874d3e8d9c0dbf0dbd8e3cfc35f4905
Sheldon <sheldon.warden@gmail.com>
2018-03-21 23:25:29 +0100

    Display common dice

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,6 +15,22 @@ export class HomePage {
     this.available_dice=[4,6,8,10,12,16,20,24,30].map(
       faces => {let die=new SimpleDice(faces);
         die.text=`d${faces}`;
+        if (die.text=="d20") {
+          die.markup='<img src="../../assets/imgs/d20.png">'
+        } else if (die.text=="d12"){
+          die.markup='<img src="../../assets/imgs/d12.png">'
+        } else if (die.text=="d10"){
+          die.markup='<img src="../../assets/imgs/d10.png">'
+        } else if (die.text=="d8"){
+          die.markup='<img src="../../assets/imgs/d8.png">'
+        } else if (die.text=="d6"){
+          die.markup='<img src="../../assets/imgs/d6.png">'
+        } else if (die.text=="d4"){
+          die.markup='<img src="../../assets/imgs/d4.png">'
+        } else {
+          die.markup=die.text
+        }
+
         return die;
       });
   }
