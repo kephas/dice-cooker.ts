57746b0bcd64c61c010768b6faa912ba8f38fec8
Pierre Thierry <pierre@nothos.net>
2018-03-21 23:47:18 +0100

    Add roll button and history

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,9 +9,13 @@ import { Dice, SimpleDice, ExplosiveDice, Sequence, FateSequence } from '../../a
 export class HomePage {
   available_dice: Dice[];
   staged_dice: Dice[];
+  rolls: [Dice[], number][];
+  staged: boolean = false;
+  rolled: boolean = false;
 
   constructor(public navCtrl: NavController) {
     this.staged_dice=[];
+    this.rolls=[];
     this.available_dice=[4,6,8,10,12,16,20,24,30].map(
       faces => {let die=new SimpleDice(faces);
         die.text=`d${faces}`;
@@ -21,7 +25,13 @@ export class HomePage {
 
   stage(die:Dice){
     this.staged_dice.push(Object.create(die));
+    this.staged = true;
   }
 
+  roll() {
+    let seq = new Sequence(this.staged_dice);
+    this.rolls.push([this.staged_dice, seq.roll()]);
+    this.rolled = true;
+  }
 
 }
